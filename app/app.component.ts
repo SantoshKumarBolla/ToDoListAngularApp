ce974022f5ce9a8842350a42d1bb893ab3a08b91
Maheshkumar <maheshkumarn@live.com>
2017-07-25 13:04:41 +0530

    Main and Index

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component(
+    {
+        selector: "app-todo",
+        template: `<div class="bg-warning">ToDo List will come here....</div>`
+    }
+)
+
+export class AppComponent {}
